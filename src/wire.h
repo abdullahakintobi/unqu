9d3fb5f921d72db1d174fd9d86aa1b960a824147
Saheed Adeleye <aadesaed@gmail.com>
2025-06-20 18:11:44 +0100

    unqud: print the wire frame

--- src/wire.h
+++ src/wire.h
@@ -23,14 +23,30 @@ there's message frame as a unit of communication:
 #pragma once
 
 #include <stdint.h>
+#include <stdio.h>
 #include "util.h"
 
 #define WIRE_VERSION 0
 #define WIRE_END_BYTE 0x44
 
+#define KIND_LIST 0
+#define KIND_KILL 1
+
+#define kind2str(kind) ( \
+	(kind) < KIND_LIST || (kind) > KIND_KILL ? \
+		"UNKNOWN" : (kind) == KIND_LIST ? \
+			"LIST" : "KILL" \
+)
+
 struct msgkill {
 	int32_t pid;
 } attr(packed);
+
+void msgkill_print(struct msgkill* mk) {
+	if (mk == NULL) return;
+
+	printf("  pid %d\n", mk->pid);
+}
 
 struct msglist {
 	uint8_t v: 1;
@@ -49,3 +65,25 @@ struct wire_frame {
 	} m;
 	uint8_t end;
 } attr(packed);
+
+void wire_frame_print(struct wire_frame* frame) {
+	if (frame == NULL) return;
+
+	printf("Frame {\n"
+		"  version %d\n"
+		"  kind %s\n",
+		frame->version, kind2str(frame->kind)
+	);
+
+	switch (frame->kind) {
+	case KIND_KILL:
+		msgkill_print(&frame->m.kill);
+		break;
+	case KIND_LIST:
+		break;
+	default:
+		break;
+	}
+
+	puts("}");
+}
